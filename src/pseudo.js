00da84c5fa951909a418d6109ad3df7b2fe8ba4a
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-09 21:08:01 +0200

    Pseudocode added and added button div to html

---
+++ src/pseudo.js
@@ -0,0 +1,20 @@
+// Pseudo Code
+
+// Create div with content id
+// Create button with new todo id
+
+// If new todo button clicked
+  // Show div which takes input
+  // Take input for title
+  // Take input for description
+  // Take input for date
+  // Add submit button
+  // When submit button clicked create new todo div under content div
+    // Add event listener to button that runs this add card div code
+    // Create card div
+    // Create title div under card
+    // Create description div under card
+    // Create date div under card
+    // Set value of input equal to that of title/description/date 
+    // Add a delete button that removes card
+      // Add event listener that runs this removal code
