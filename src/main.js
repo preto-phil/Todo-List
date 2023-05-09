0b7d3408e0594f4a048d7bf0ea9dd83a06633335
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-09 20:21:24 +0200

    Dist and src folders added, hmtl boilerplate and files linked, js console code

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const obj = (title, description, date) => {
+  const getTitle = () => console.log(`Title: ${title}`);
+  const getDescription = () => console.log(`Description: ${description}`);
+  const getDate = () => console.log(`Date: ${date}`);
+  const getCard = () => console.log(`${title}, ${description}, ${date}`)
+
+  return {
+    getTitle,
+    getDescription,
+    getDate,
+    getCard
+  }
+}
+
+const task1 = obj('Guitar Practice', 'Practice G an C chord for 15 min, followed by 15min free play', '19:00, 09 May 2013');
+
+task1.getTitle();
+task1.getDescription();
+task1.getDate();
+task1.getCard();
