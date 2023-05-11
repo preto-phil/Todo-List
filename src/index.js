a3952c292c508ac82d248f6d3129317a9b2767d5
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-11 23:33:56 +0200

    A lil bit of progress

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import openGithub from './github';
 
 // Dropdown for add
 
-function newTodoDiv() {
-  const newTodo = document.getElementById('new-todo-btn');
-  newTodo.addEventListener('click', () => {
+function toggleNewTodo() {
+  const newTodoBtn = document.getElementById('new-todo-btn');
+  newTodoBtn.addEventListener('click', () => {
     const add = document.getElementById('add');
-    add.style.display = 'grid';
+    if (add.style.display === 'none') {
+      add.style.display = 'grid';
+      newTodoBtn.innerText = 'Close'
+    } else if (add.style.display === 'grid') {
+      add.style.display = 'none';
+    }
   });
 }
 
-newTodoDiv();
+toggleNewTodo();
 
 
 // Click on github icon to open github
