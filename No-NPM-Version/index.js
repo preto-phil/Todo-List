041494671abc4acdc5a9eec3293574199f00d411
Philip Pretorius <philippretorius.79@gmail.com>
2023-05-23 20:17:26 +0200

    No NPM version added

---
+++ No-NPM-Version/index.js
@@ -0,0 +1,145 @@
+let data = [];
+
+// Dropdown for add
+
+function toggleNewTodo() {
+  const newTodoBtn = document.getElementById('new-todo-btn');
+  newTodoBtn.addEventListener('click', () => {
+    const add = document.getElementById('add');
+    if (add.style.display === 'none') {
+      add.style.display = 'grid';
+      newTodoBtn.innerText = 'Close';
+    } else if (add.style.display === 'grid') {
+      add.style.display = 'none';
+      newTodoBtn.innerText = 'New Todo';
+    }
+  });
+}
+
+toggleNewTodo();
+
+
+// Store data
+function storeData() {
+  const title = document.getElementById('title');
+  const description = document.getElementById('description');
+  const date = document.getElementById('date');
+
+  data.push({
+    title: title.value,
+    description: description.value,
+    date: date.value,
+  });
+
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+
+// Build card on add click
+
+function addTodo() {
+  const addTodoBtn = document.getElementById('add-todo');
+  addTodoBtn.addEventListener('click', () => {
+    storeData();
+    createTodo();
+    resetForm();
+  })
+}
+
+let createTodo = () => {
+  const contentDiv = document.getElementById('content');
+  contentDiv.replaceChildren();
+  data.map((x, y) => {
+    return (contentDiv.innerHTML += `
+    <div id=${y} class="createdDiv">
+      <div class="titleDiv">${x.title}</div>
+      <div class="desDiv">${x.description}</div>
+      <div class="date.div">${x.date}</div>
+
+      <div class="options">
+        <i class="fas fa-edit edit"></i>
+        <i class="fas fa-trash-alt dlt"></i>
+      </div>
+    </div>
+    `);
+  });
+};
+
+/* function editFunction() {
+  let editDiv = document.querySelectorAll(".edit");
+  for (let i = 0; i < editDiv.length; i++) {
+    editDiv[i].onclick = (e) => {
+      editTodo(e);
+      console.log('Yolo')
+    };
+  }
+}   */
+
+/* function dltFunction() {
+  const dltDiv = document.querySelectorAll('.dlt');
+  for (let i = 0; i < dltDiv.length; i++) {
+    dltDiv[i].onclick = (e) => {
+      deleteTodo(e);
+      createTodo();
+      console.log('Yolo Once')
+    };
+  }
+} */
+
+const dltDiv = document.querySelectorAll('.dlt');
+for (let i = 0; i < dltDiv.length; i++) {
+  dltDiv[i].onclick = (e) => {
+    alert('Hos');
+    console.log('Yolo Once')
+  };
+}
+
+
+/* editFunction();
+dltFunction();
+ */
+/// Reset form
+
+function resetForm() {
+  title.value = "";
+  description.value = "";
+  date.value = "";
+};
+
+// Delete todo function
+
+function deleteTodo(e) {
+  e.parentElement.parentElement.remove();
+
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+};
+
+// Edit todo function
+
+let editTodo = (e) => {
+  let selectedTodo = e.parentElement.parentElement;
+
+  title.value = selectedTodo.children[0].innerHTML;
+  description.value = selectedTodo.children[1].innerHTML;
+  date.value = selectedTodo.children[2].innerHTML;
+
+  deleteTodo(e);
+};
+
+
+// Add todo
+addTodo();
+
+// Click on github icon to open github
+function openGithub() {
+  const githubIcon = document.getElementById('github-icon');
+  githubIcon.addEventListener('click', () => {
+    window.open('https://github.com/preto-phil');
+});
+}
+
+openGithub();
